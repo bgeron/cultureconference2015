388ae1b8ad6aa52cf25558fe6a373bb22609cfdf
Bram Geron <bgeron@bgeron.nl>
2015-07-11 00:52:08 +0200

    wget --mirror -p -k --timeout=10 cultureconference2015.com

---
+++ cultureconference2015.com/wp-login.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="__customer_validation/style.css">
+		<style>
+			.top-swoosh > .swoosh
+			{
+				height: 15px;
+				padding: 0;
+			}
+			div.content
+			{
+				font-family: Helvetica Neue, Arial;
+				font-size: 13px;
+			}
+			h1
+			{
+				font-family: Helvetica Neue, Arial;
+				font-size: 24px;
+			}
+		</style>
+		<script type="text/javascript">
+		var RecaptchaOptions = {
+			theme : 'white'
+		};
+		</script>
+	</head>
+	<body>
+		<div id="hidden-title-eu" style="display:none;">Pre-emptive security measure</div>
+		<div id="hidden-title-nl" style="display:none;">Preventieve veiligheidsmaatregel</div>
+		<div id="hidden-title-be" style="display:none;">Preventieve veiligheidsmaatregel</div>
+		<div id="hidden-title-de" style="display:none;">Vorbeugende Sicherheitsma&szlig;nahme</div>
+
+		<div id="all">
+			<div id="header">
+				<ul class="languages">
+					<li class="de"><a href="javascript:showForCountry('de');"><span>Deutsch</span></a></li>
+					<li class="nl"><a href="javascript:showForCountry('nl');"><span>Nederlands</span></a></li>
+					<li class="eu"><a href="javascript:showForCountry('eu');"><span>English</span></a></li>
+					<li class="be"><a href="javascript:showForCountry('be');"><span>Nederlands</span></a></li>
+				</ul>
+			</div>
+
+			<div class="main" id="">
+				<div class="container top-swoosh">
+					<div class="swoosh">
+					</div>
+					<div class="content" id="content">
+						<div class="country-content" id="country-eu">
+							<h2>Pre-emptive security measure</h2>
+							The number of attacks of so-called botnets on WordPress and Joomla website has been increased lately. We recently released this free pre-emptive security measure, to protect you against these attacks. By completing this validation your website will no longer be susceptible for the attacks.<br /><br />
+							Please retype the following words and click 'Continue'.<br /><br />
+						</div>
+						<div class="country-content" id="country-nl">
+							<h2>Preventieve veiligheidsmaatregel</h2>
+							Steeds vaker zijn WordPress en Joomla websites het doelwit van aanvallen door zogenaamde botnets. Als extra bescherming tegen dergelijke aanvallen hebben wij deze gratis preventieve beveiliging ingebouwd. Door het voltooien van deze validatie is uw website niet langer vatbaar voor deze aanvallen. <br /><br />
+							Tik de twee woorden hieronder over en klik op 'doorgaan'.<br /><br />
+						</div>
+						<div class="country-content" id="country-be">
+							<h2>Preventieve veiligheidsmaatregel</h2>
+							Steeds vaker zijn WordPress en Joomla websites het doelwit van aanvallen door zogenaamde botnets. Als extra bescherming tegen dergelijke aanvallen hebben wij deze gratis preventieve beveiliging ingebouwd. Door het voltooien van deze validatie is uw website niet langer vatbaar voor deze aanvallen. <br /><br />
+							Tik de twee woorden hieronder over en klik op 'doorgaan'.<br /><br />
+						</div>
+						<div class="country-content" id="country-de">
+							<h2>Vorbeugende Sicherheitsma&szlig;nahme</h2>
+							Mit zunehmender Häufigkeit werden WordPress und Joomla Webseiten zum Ziel für Angriffe durch sogenannte Botnet.<br />
+ Als zusätzlichen Schutz vor solchen Angriffen haben wir diese kostenlose Sicherheitsmaßnahme eingebaut. Durch die Ausführung dieser Kontrolle ist Ihre Webseite nicht mehr anfällig für derartige Attack.<br /><br />
+							Geben Sie die beiden unten stehenden Wörter ein und klicken Sie auf 'Weiter'.<br /><br />
+						</div>
+
+						<form method="post" action="http://cultureconference2015.com/__customer_validation/validate.php">
+							<script type="text/javascript" src="http://www.google.com/recaptcha/api/challenge?k=6Lf38-cSAAAAAET3XX62pJHRVCOw7xKW274ermGb"></script>
+							<noscript>
+								<iframe src="http://www.google.com/recaptcha/api/noscript?k=6Lf38-cSAAAAAET3XX62pJHRVCOw7xKW274ermGb" height="300" width="500" frameborder="0"></iframe><br />
+								<textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
+								<input type="hidden" name="recaptcha_response_field" value="manual_challenge">
+							</noscript>
+							<input type="hidden" name="ref" value="cultureconference2015.com"/>
+							<input type="hidden" name="uri" value="/wp-login.php"/>
+							<div class="country-content" id="country-button-eu">
+								<input type="submit" value="Continue" style="float: right; text-align: right;"/>
+							</div>
+							<div class="country-content" id="country-button-nl">
+								<input type="submit" value="Doorgaan" style="float: right; text-align: right;"/>
+							</div>
+							<div class="country-content" id="country-button-be">
+								<input type="submit" value="Doorgaan" style="float: right; text-align: right;"/>
+							</div>
+							<div class="country-content" id="country-button-de">
+								<input type="submit" value="Weiter" style="float: right; text-align: right;"/>
+							</div>
+							
+						</form>
+					</div>
+					<div class="bottom"></div>
+				</div>
+			</div>
+
+		</div>
+
+		<script type="text/javascript" src="__customer_validation/script.js"></script>
+	</body>
+</html>
+
